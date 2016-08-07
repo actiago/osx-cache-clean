3fc97ea9c3bea88db456aa32ce3b9e9c34280180
tiago <tiagoamaralc@gmail.com>
2016-08-07 02:27:27 -0300

    Melhorias

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # OSX Cache Clean  
-###### versão 0.0.1.0
-###### Limpe arquivos de "cache" de aplicativos de seu OSX e ganhe espaço e desempenho!
+###### versão 0.0.1.1
+###### Limpe arquivos de "cache" de aplicativos de seu OSX! Ganhe espaço e desempenho!
 =============================
 
 
-## Como utilizar a ferramenta: 
-1. Salve o arquivo osxcc.sh em seu computador
+## Como utilizar a ferramenta:
+1. Baixe o arquivo __osxcc.sh__ 
 2. Abra o Terminal
 3. Navegue até o diretório que contém o arquivo baixado. 
-4. Dê permissões de execução com o seguinte comando: >$ chmod +x osxcc.sh 
-5. Execute o script: >$ ./osxcc.sh 
-6. Aproveite :D 
+4. Dê permissões de execução com o seguinte comando: $ chmod +x osxcc.sh
+5. Execute o script: $ ./osxcc.sh 
+6. Aproveite :smiley: 
 
--- osxcc.sh
+++ osxcc.sh
@@ -1,26 +1,30 @@
+#!/bin/bash
+
 # Script simples de limpeza de armazenamento temporário OSX
+# Elaborado por Tiago Alves https://cegohub.gitbub.io 
+
+# Variáveis
+INI=`du -hs ~/Library/Caches | awk '{ print $1}'`
 
 echo "Verificando tamanho do espaço utilizado no diretório de cache" 
-
-	du -hs ~/Library/Caches
-
-echo "Deseja apagar arquivos de cache? s/N"
+echo "#"
+sleep 1
+echo "#"
+sleep 1
+echo "#"
+sleep 1
+echo "#"
+sleep 1
+echo "Deseja excluir $INI de dados em seu dispositivo? s/N"
 read OPCAO
 	
 	if [ $OPCAO == 's' ]; then 
 
  		echo "Excluindo arquivos"
-
-		cd ~/Library/Caches
-		rm -rf *
-	 
-		echo "Verificando armazenamento"
-		
-		sleep 2
-		du -hs ~/Library/Caches
+			cd ~/Library/Caches
+			rm -rf * 2>/dev/null
 		sleep 1
-
-		echo "Limpeza concluida"
+		echo "Você liberou $INI de dados!"
 	
 	else 
 		echo "Arquivos não excluídos" 
