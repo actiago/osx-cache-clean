5d83e12c36c3e1a89ff52da7b7095ec29fe04751
cegolab <tiagoamaralc@gmail.com>
2019-01-11 00:23:13 -0200

    Melhorias no texto - PT-BR

--- osxcc.sh
+++ osxcc.sh
@@ -1,27 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Script simples de limpeza de armazenamento temporário OSX
-# Elaborado por Tiago Alves https://cegohub.gitbub.io 
+# Author https://github.com/cegohub 
 
-# Variáveis
-INI=`du -hs ~/Library/Caches | awk '{ print $1}'`
-			
-# Execução 
-echo "Verificando tamanho do espaço utilizado no diretório de cache" 
-sleep 2
-clear
+# Vars
+INI=`du -hsc ~/Library/Caches | awk '{ print $1}'`
+AvailableSpace=$(df -h / | tail -1 | awk '{print $4}')
+
+# Run 
+echo "Verificando o volume de dados contifos no diretório de cache de seu usuário" 
+echo " "
+echo "Espaço atual disponível em disco $AvailableSpace"
 read -p "Deseja excluir $INI de dados em seu dispositivo? s/N " -n2 OPCAO
-echo "#"
-sleep 1
-	if [ $OPCAO == 's' ]; then 
+echo " "
 
+	if [ $OPCAO == 's' ]; then 
  		echo "Excluindo arquivos"
 			cd ~/Library/Caches
 			rm -rf * 2>/dev/null
-		sleep 1
-		echo "#"
-		echo "Você liberou $INI de dados!"
-	
+		echo " "
+		echo "Você liberou $INI de dados e seu disco agora tem $AvailableSpace"
 	else 
-		echo "Arquivos não excluídos" 
-	fi 
+		echo "Você optou por não remover os arquivos." 
+	fi
