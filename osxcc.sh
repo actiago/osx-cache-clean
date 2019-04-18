0a22593981a4992ec19ffe3f556f5a2d99c55e2b
Tiago Amaral <tiago.amaral@rivendel.com.br>
2019-04-17 21:37:42 -0300

    Fix identations

--- osxcc.sh
+++ osxcc.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 
-# Author https://github.com/cegohub 
+# Author https://github.com/cegohub
 
 # Vars
 INI=`du -hsc ~/Library/Caches | awk 'NR==1{print $1}'`
 AvailableSpace=$(df -h / | tail -1 | awk '{print $4}')
 
-# Run 
-echo "* Verifying files in apps cache directory" 
+# Run
+echo "* Verifying files in apps cache directory"
 echo " "
 echo "* Available space in the disk $AvailableSpace"
 echo " "
 read -p "* Do you want to remove $INI of space in your disk? y/N: " -n2 OPTION
 echo " "
 
-	if [ $OPTION == 'y' ]; then 
+	if [ $OPTION == 'y' ]; then
  		echo "Deleting files"
 			cd ~/Library/Caches
 			rm -rf * 2>/dev/null
 		echo " "
 		echo "You removed $INI of files and now you have $AvailableSpace of available space in your disk. Thank you!"
-	else 
-		echo "Canceled." 
+	else
+		echo "Canceled."
 	fi
