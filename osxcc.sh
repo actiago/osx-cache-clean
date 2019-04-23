ed43481a3a818bda55b471fa7375fac3672d0f4c
Tiago Amaral <tiago.amaral@rivendel.com.br>
2019-04-22 23:24:48 -0300

    Fix 'CacheSize' variable

--- osxcc.sh
+++ osxcc.sh
@@ -3,7 +3,7 @@
 # Author Tiago Amaral https://github.com/cegohub
 
 # Vars
-CacheSize="du -hsc ~/Library/Caches | awk 'NR==1{print $1}'"
+CacheSize="$(du -hsc ~/Library/Caches | awk 'NR==1{print $1}')"
 AvailableSpace="$(df -h / | tail -1 | awk '{print $4}')"
 
 # Run
